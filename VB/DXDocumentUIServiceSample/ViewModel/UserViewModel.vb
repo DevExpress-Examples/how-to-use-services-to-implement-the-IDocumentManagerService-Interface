12646f57adf279345b2a61220ca70e46971a3b19
Anthony Sam <anton.sameishchev@devexpress.com>
2020-12-29 17:27:19 +0300

    Update UserViewModel.vb

--- VB/DXDocumentUIServiceSample/ViewModel/UserViewModel.vb
+++ VB/DXDocumentUIServiceSample/ViewModel/UserViewModel.vb
@@ -1,4 +1,4 @@
-﻿Imports DevExpress.Mvvm
+Imports DevExpress.Mvvm
 Imports DevExpress.Mvvm.DataAnnotations
 Imports DXDocumentUIServiceSample.Common
 Imports System
@@ -6,92 +6,105 @@ Imports System.Collections.ObjectModel
 Imports System.ComponentModel
 
 Namespace DXDocumentUIServiceSample.ViewModel
-	Public Class UserViewModel
-		Inherits ViewModelBase
-		Implements IDocumentContent
 
-		Public Property ID() As Integer
-			Get
-				Return GetValue(Of Integer)()
-			End Get
-			Set(ByVal value As Integer)
-				SetValue(value)
-			End Set
-		End Property
-		Public Property NickName() As String
-			Get
-				Return GetValue(Of String)()
-			End Get
-			Set(ByVal value As String)
-				SetValue(value)
-			End Set
-		End Property
-		Public Property Registration() As DateTime
-			Get
-				Return GetValue(Of DateTime)()
-			End Get
-			Set(ByVal value As DateTime)
-				SetValue(value)
-			End Set
-		End Property
-		Public Property Rating() As Decimal
-			Get
-				Return GetValue(Of Decimal)()
-			End Get
-			Set(ByVal value As Decimal)
-				SetValue(value)
-			End Set
-		End Property
+    Public Interface IDocumentContent
+        Sub OnClose(e As CancelEventArgs)
+        Sub OnDestroy()
+    End Interface
 
 
+    Public Class UserViewModel
+        Inherits ViewModelBase
+        Implements IDocumentContent
 
-		Public Property GlobalUserActivity() As ObservableCollection(Of ActionsPerMonth)
-			Get
-				Return GetValue(Of ObservableCollection(Of ActionsPerMonth))()
-			End Get
-			Set(ByVal value As ObservableCollection(Of ActionsPerMonth))
-				SetValue(value)
-			End Set
-		End Property
+        Public Property ID() As Integer
+            Get
+                Return GetValue(Of Integer)()
+            End Get
+            Set(ByVal value As Integer)
+                SetValue(value)
+            End Set
+        End Property
+        Public Property NickName() As String
+            Get
+                Return GetValue(Of String)()
+            End Get
+            Set(ByVal value As String)
+                SetValue(value)
+            End Set
+        End Property
+        Public Property Registration() As DateTime
+            Get
+                Return GetValue(Of DateTime)()
+            End Get
+            Set(ByVal value As DateTime)
+                SetValue(value)
+            End Set
+        End Property
+        Public Property Rating() As Decimal
+            Get
+                Return GetValue(Of Decimal)()
+            End Get
+            Set(ByVal value As Decimal)
+                SetValue(value)
+            End Set
+        End Property
 
 
 
-		Public Property LocalUserActivity() As ObservableCollection(Of ActionsPerMonth)
-			Get
-				Return GetValue(Of ObservableCollection(Of ActionsPerMonth))()
-			End Get
-			Set(ByVal value As ObservableCollection(Of ActionsPerMonth))
-				SetValue(value)
-			End Set
-		End Property
-
-		<Command>
-		Public Sub Close()
-			DocumentOwner.Close(Me, False)
-		End Sub
+        Public Property GlobalUserActivity() As ObservableCollection(Of ActionsPerMonth)
+            Get
+                Return GetValue(Of ObservableCollection(Of ActionsPerMonth))()
+            End Get
+            Set(ByVal value As ObservableCollection(Of ActionsPerMonth))
+                SetValue(value)
+            End Set
+        End Property
 
 
 
-		#Region "IDocumentContent"
-		Public Property DocumentOwner() As IDocumentOwner
+        Public Property LocalUserActivity() As ObservableCollection(Of ActionsPerMonth)
+            Get
+                Return GetValue(Of ObservableCollection(Of ActionsPerMonth))()
+            End Get
+            Set(ByVal value As ObservableCollection(Of ActionsPerMonth))
+                SetValue(value)
+            End Set
+        End Property
+
+        <Command>
+        Public Sub Close()
+            DocumentOwner.Close(CType(Me, DevExpress.Mvvm.IDocumentContent), False)
+        End Sub
 
 
 
-		Public Sub OnClose(ByVal e As System.ComponentModel.CancelEventArgs)
-		End Sub
+#Region "IDocumentContent"
+        Public Property DocumentOwner() As IDocumentOwner
 
 
 
-		Public Sub OnDestroy()
-		End Sub
+        Public Sub OnClose(ByVal e As System.ComponentModel.CancelEventArgs)
+        End Sub
 
 
 
-		Public ReadOnly Property Title() As Object
-			Get
-				Return String.Format("{1} ({2})", ID, NickName, Registration.ToShortDateString())
-			End Get
-		End Property
-		#End Region
-	End Class
+        Public Sub OnDestroy()
+        End Sub
+
+        Private Sub IDocumentContent_OnClose(e As CancelEventArgs) Implements IDocumentContent.OnClose
+            Throw New NotImplementedException()
+        End Sub
+
+        Private Sub IDocumentContent_OnDestroy() Implements IDocumentContent.OnDestroy
+            Throw New NotImplementedException()
+        End Sub
+
+        Public ReadOnly Property Title() As Object
+            Get
+                Return String.Format("{1} ({2})", ID, NickName, Registration.ToShortDateString())
+            End Get
+        End Property
+#End Region
+    End Class
 End Namespace
